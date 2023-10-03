ddad93e062bdf417f8937b8c50d8099ae13726ff
anilpirlaccbp <anilpirla224@gmail.com>
2023-10-03 06:00:50 +0000

    all test cases passed

---
+++ src/components/Logout/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const Logout = props => {
+  const {logout} = props
+
+  return (
+    <button type="button" onClick={logout} className="logout-button">
+      Logout
+    </button>
+  )
+}
+
+export default Logout
